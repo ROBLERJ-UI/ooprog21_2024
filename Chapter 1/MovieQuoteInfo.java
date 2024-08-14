b67b2c1ab82b27aa8648912f78eae78fabc5a23e
ROBLERJ-UI <rjroble2921@gmail.com>
2024-08-15 00:03:50 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+public class MovieQuoteInfo { 
+    public static void displayQuoteInfo(){
+        String quote = "I'm gonna make him an offer that he cannot refuse.";
+        String movie = "The Godfather";
+        String character = "Godfather";
+        int year = 1972;
+        System.out.println("Quote: " + quote);
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    } 
+    public static void main(String[] args) {
+   displayQuoteInfo();
+    }
+}
